74ee30bdb5e4855cd518368c268a0ab7d62111f3
Andy Rusiecki <andy.rusiecki@gmail.com>
2023-04-06 17:04:04 -0400

    combining scripts

--- arch/desktop/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-modules=(
-  './fonts/install.sh'
-)
-
-for module in "${modules[@]}" ; do
-  source $(realpath $module)
-done
--- arch/device/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-modules=(
-  './framework/install.sh'
-)
-
-for module in "${modules[@]}" ; do
-  source $(realpath $module)
-done
